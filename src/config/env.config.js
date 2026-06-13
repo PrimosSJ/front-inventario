50653a6126bbeca1e91d537cc007d1d3703dfc52
AlvinIsCrack <animations468@gmail.com>
2026-06-13 17:11:58 -0400

    fix(config): update API_URL comment to include alternative backend URL

--- src/config/env.config.js
+++ src/config/env.config.js
@@ -3,6 +3,6 @@
  * Validates and exposes environment variables to prevent hardcoded values across the app.
  */
 export const ENV = {
-    API_URL: import.meta.env.VITE_API_URL || "http://localhost:4000",
+    API_URL: import.meta.env.VITE_API_URL || "http://localhost:4000", /*"https://poto-back.inf.santiago.usm.cl"*/
     IGNORE_AUTH: import.meta.env.VITE_IGNORE_AUTH_BACKEND === "true",
 };
